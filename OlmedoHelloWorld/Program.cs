24879ba8eb98705aaee571ca3393a5567ba2da1d
Dennis Olmedo <dolmedo837@gmail.com>
2021-01-30 14:43:21 -0500

    PE1 Hello World

---
+++ OlmedoHelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlmedoHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables as needed
+            string testString = "Hello World!";
+            int testNum = 10;
+
+            //Here I print out both of my variables
+            Console.WriteLine(testString + " My name is Dennis Olmedo");
+            Console.WriteLine(testNum);
+
+            //This loop subtracts 1 from testNum until it reaches 0
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(testNum);
+                testNum -= 1;
+                
+            }
+        }
+    }
+}
