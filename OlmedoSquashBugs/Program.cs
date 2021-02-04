78db76171548b11438efc5190a98dcab108d046c
Dennis Olmedo <dolmedo837@gmail.com>
2021-02-03 21:26:11 -0500

    PE2 *Updated*

--- OlmedoSquashBugs/Program.cs
+++ OlmedoSquashBugs/Program.cs
@@ -20,20 +20,21 @@ namespace OlmedoSquashBugs
         static void Main(string[] args)
         {
             // declare int counter
-            //int i = 0
-
+            //int i = 0 Compile-Time Error missing a semicolon
+            int i = 0;
+            string allNumbers = null;
             // loop through the numbers 1 through 10
-            for (int i = 1; i <= 10; ++i)
+            for ( i = 1; i <= 10; ++i)
             {
                 // declare string to hold all numbers
-                string allNumbers = null;
+                //string allNumbers = null; Logic Error our variables should be initialized outside of the loop so that we can display the numbers processed at the end
 
                 // output explanation of calculation
-                //Console.Write(i + "/" + i - 1 + " = ");
+                //Console.Write(i + "/" + i - 1 + " = "); Compile-Time Error, it is missing a set of parentheses for i - 1 to work
 
                 Console.Write(i + "/" + (i - 1) + " = ");
                 // output the calculation based on the numbers
-                //Console.WriteLine(i / (i - 1));
+                //Console.WriteLine(i / (i - 1)); Run-Time Error because you would be dividing by zero
 
                 try
                 {
@@ -51,11 +52,13 @@ namespace OlmedoSquashBugs
                 // increment the counter
                 //i = i + 1;
 
-                Console.WriteLine("These numbers have been processed " + allNumbers);
             }
 
+
+
             // output all numbers which have been processed
-            //Console.WriteLine("These numbers have been processed: " allNumbers);
+            //Console.WriteLine("These numbers have been processed: " allNumbers); Compile Time Error there should be a plus sign to concatenate the text with the allNumbers string
+            Console.WriteLine("These numbers have been processed " + allNumbers);
         }
     }
 }
