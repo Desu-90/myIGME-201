1f1be98ccb6ad013247ec4c7d9acb249b9545a09
Dennis Olmedo <dolmedo837@gmail.com>
2021-02-08 20:10:49 -0500

    PE4

---
+++ PE4/Program.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int input1;
+            int input2;
+            bool check = false;
+            Console.WriteLine("Please enter an integer that is less than 10");
+            while (!check)
+            {
+                try
+                {
+                    
+                    input1 = Convert.ToInt32(Console.ReadLine());
+                    check = input1 < 10;
+                    Console.WriteLine("Please enter another integer that is less than 10");
+                }
+                catch
+                {
+                    Console.WriteLine("Please try again");
+
+                }
+                
+            }*/
+            
+
+        }
+    }
+}
+
