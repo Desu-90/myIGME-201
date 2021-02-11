05121b006210a4112d1c0a1203e575053a816942
Dennis Olmedo <dolmedo837@gmail.com>
2021-02-10 22:42:17 -0500

    PE 6

--- Mandelbrot/Program.cs
+++ Mandelbrot/Program.cs
@@ -33,40 +33,75 @@ namespace Mandelbrot
             double realCoord, imagCoord;
             double realTemp, imagTemp, realTemp2, arg;
             int iterations;
-            for (imagCoord = 1.2; imagCoord >= -1.2; imagCoord -= 0.05)
+
+            double input1 = -1;
+            double input2 = -1;
+            bool bValid = false;
+
+            try
             {
-                for (realCoord = -0.6; realCoord <= 1.77; realCoord += 0.03)
+                string sResponse = Console.ReadLine();
+                Int32 nResponse = int.Parse(sResponse);
+                bValid = true;
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer.");
+                bValid = false;
+            }
+            finally
+            {
+                Console.WriteLine("This line will always be output!");
+            }
+
+            /*do
+            {
+                Console.Write("Please enter a start and end value:");
+                bValid = double.TryParse(Console.ReadLine(), out input1);
+
+                Console.WriteLine("Please enter an end value");
+                bValid = double.TryParse(Console.ReadLine(), out input2);  
+            } while (!bValid);*/
+
+            Console.WriteLine();
+
+            if (input1 >= 0) {
+
+                for (imagCoord = input1; imagCoord >= -(input2); imagCoord -= 0.05)
                 {
-                    iterations = 0;
-                    realTemp = realCoord;
-                    imagTemp = imagCoord;
-                    arg = (realCoord * realCoord) + (imagCoord * imagCoord);
-                    while ((arg < 4) && (iterations < 40))
+                    for (realCoord = -.6; realCoord <= 1.77; realCoord += 0.03)
                     {
-                        realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
-                           - realCoord;
-                        imagTemp = (2 * realTemp * imagTemp) - imagCoord;
-                        realTemp = realTemp2;
-                        arg = (realTemp * realTemp) + (imagTemp * imagTemp);
-                        iterations += 1;
+                        iterations = 0;
+                        realTemp = realCoord;
+                        imagTemp = imagCoord;
+                        arg = (realCoord * realCoord) + (imagCoord * imagCoord);
+                        while ((arg < 4) && (iterations < 40))
+                        {
+                            realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
+                               - realCoord;
+                            imagTemp = (2 * realTemp * imagTemp) - imagCoord;
+                            realTemp = realTemp2;
+                            arg = (realTemp * realTemp) + (imagTemp * imagTemp);
+                            iterations += 1;
+                        }
+                        switch (iterations % 4)
+                        {
+                            case 0:
+                                Console.Write(".");
+                                break;
+                            case 1:
+                                Console.Write("o");
+                                break;
+                            case 2:
+                                Console.Write("O");
+                                break;
+                            case 3:
+                                Console.Write("@");
+                                break;
+                        }
                     }
-                    switch (iterations % 4)
-                    {
-                        case 0:
-                            Console.Write(".");
-                            break;
-                        case 1:
-                            Console.Write("o");
-                            break;
-                        case 2:
-                            Console.Write("O");
-                            break;
-                        case 3:
-                            Console.Write("@");
-                            break;
-                    }
+                    Console.Write("\n");
                 }
-                Console.Write("\n");
             }
 
         }
---
+++ PE5 Olmedo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE5_Olmedo
+{    // Class: Program
+    // Author: Dennis Olmedo
+    // Purpose: Parsing and formatting practice exercise
+    // Restrictions: None
+    static class Program
+    {
+        //Method: Main
+        //Purpose: Parsing and Formatting practice
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            //Declaring our variables
+            Random rand = new Random();
+
+            // generate a random number between 0 inclusive and 101 exclusive
+            int randomNumber = rand.Next(0, 101);
+
+            int input;
+            Console.WriteLine(randomNumber);
+            int i;
+            Console.WriteLine("Please enter a number between 1 and 100");
+
+            for (i = 1; i <= 8; i++)
+            {
+                //Prompt user for input
+                Console.Write("Turn #" + (i) + ": ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input < randomNumber)
+                {
+                    Console.WriteLine("Go a little higher!");
+                }
+                else if (input == randomNumber)
+                {                  
+                        Console.WriteLine("Wohoo you got it right! Took ya " + (i) + " turns");
+                        break;                    
+                    
+                }               
+                else
+                {
+                    Console.WriteLine("Woah there! Go down more");
+                }
+                
+            }
+            Console.WriteLine("Amount of guesses: " + i);
+
+        }
+    }
+}
