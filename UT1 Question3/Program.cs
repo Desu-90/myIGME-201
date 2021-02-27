cbfd287b9df764d68ed7863f584455dc5cb04c7a
Dennis Olmedo <dolmedo837@gmail.com>
2021-02-26 20:13:07 -0500

    UT1 Question 3

---
+++ UT1 Question3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT1_Question3
+{
+    class Program
+    {
+        //we define the delegate function data type
+        delegate string MyReadLine();
+        static void Main(string[] args)
+        {
+            MyReadLine myReadLine;
+            myReadLine = new MyReadLine(Console.ReadLine);
+
+            string sString = myReadLine();
+        }
+    }
+}
