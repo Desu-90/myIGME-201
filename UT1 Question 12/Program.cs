20823146a80f775fcae070c9e43c7e62bb2974f3
Dennis Olmedo <dolmedo837@gmail.com>
2021-02-26 20:36:44 -0500

    UT1Question 12

---
+++ UT1 Question 12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT1_Question_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+            bool bValid = false;
+
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Enter your name");
+
+                sName = Console.ReadLine();
+                GiveRaise(sName, ref dSalary);
+
+                if(sName.Length > 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Congrats {0} you received a raise! Your current salary is {1}", sName, dSalary);
+        }
+
+        static bool GiveRaise( string name, ref double salary)
+        {
+            if(name == name)
+            {
+                salary += 19999;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
