9bd80b3bf1da60602b90b36df9c19a32a464a4f3
Dennis Olmedo <dolmedo837@gmail.com>
2021-03-02 12:16:32 -0500

    In-class exercise March2

---
+++ PeopleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleLib;
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People peopleList = new People();
+
+            Student student = new Student();
+
+            student.email = "d@gmail.com";
+
+            peopleList[student.email] = student;
+
+            List<int> intList = new List<int>;
+
+            foreach(int i in intList)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach(KeyValuePari<string,Person> keyValuePair in peopleList)
+            {
+                //keyValuePair.Key is the email string
+                //keyValuePair.Value is the Person object
+                Person thisPerson;
+                thisPerson = keyValuePair.Value;
+
+                if(thisPerson.GetType() == typeof(Student))
+                {
+                    student thisStudent;
+                    student = (Student)thisPerson;
+                    thisStudent.gpa = 4.0;
+                    thisStudent.Party();
+
+                }
+            }
+        }
+    }
+}
