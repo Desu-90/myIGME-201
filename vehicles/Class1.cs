013b14e9455680aeba4db35b3fc2bfcc1d7249ee
Dennis Olmedo <dolmedo837@gmail.com>
2021-03-03 17:38:53 -0500

    Vehicles PE

---
+++ vehicles/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger()
+        {
+
+        }
+    }
+    public abstract class Car : Vehicle
+    {
+    }
+    public abstract class Train : Vehicle
+    {
+    }
+    public interface IPassengerCarrier
+    {
+    }
+    public interface IHeavyLoadCarrier
+    {
+    }
+    public class SUV : Car, IPassengerCarrier
+    {
+    }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+    }
+    public class Compact : Car, IPassengerCarrier
+    {
+    }
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+    }
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+    }
+}
