3266bfa8e8bc9a4894cb70c93bd607b661333d5a
Dennis Olmedo <dolmedo837@gmail.com>
2021-03-03 19:01:30 -0500

    PE11 Ex6

--- Traffic/Program.cs
+++ Traffic/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using vehicles;
 
 namespace Traffic
 {
@@ -10,6 +11,16 @@ namespace Traffic
     {
         static void Main(string[] args)
         {
+            AddPassenger(new Compact());
+            AddPassenger(new SUV());
+            AddPassenger(new Pickup());
+            AddPassenger(new PassengerTrain());
+        }
+
+        static void AddPassenger(IPassengerCarrier Vehicle)
+        {
+            Console.WriteLine(Vehicle.ToString());
+
         }
     }
 }
