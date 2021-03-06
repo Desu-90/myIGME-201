3deabbd92dab8d9bdc0599d03cec13d5e805a1bc
Dennis Olmedo <dolmedo837@gmail.com>
2021-03-06 14:32:28 -0500

    PE12Ex3

---
+++ PE12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12
+{
+    public class MyClass
+    {
+        private string myString;
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + "(output from derived class)";
+        }
+
+        static void Main(string[] args)
+        {
+            MyDerivedClass output = new MyDerivedClass();
+            Console.WriteLine(output);
+        }
+    }
+    
+}
