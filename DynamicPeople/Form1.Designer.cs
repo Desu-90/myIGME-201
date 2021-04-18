441c23aec0067102dd9ed32c66483b3d2ebd34ea
Dennis Olmedo <dolmedo837@gmail.com>
2021-04-17 21:49:25 -0400

    PE 25

--- DynamicPeople/Form1.Designer.cs
+++ DynamicPeople/Form1.Designer.cs
@@ -30,12 +30,197 @@ namespace DynamicPeople
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.teacherButton = new System.Windows.Forms.Button();
+            this.studentButton = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.photoGroupBox = new System.Windows.Forms.GroupBox();
+            this.photoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.flowLayoutPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.toolStrip.SuspendLayout();
+            this.photoGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.studentButton);
+            this.splitContainer1.Panel1.Controls.Add(this.teacherButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 113;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // teacherButton
+            // 
+            this.teacherButton.ImageKey = "teacher.png";
+            this.teacherButton.ImageList = this.imageList;
+            this.teacherButton.Location = new System.Drawing.Point(8, 37);
+            this.teacherButton.Name = "teacherButton";
+            this.teacherButton.Size = new System.Drawing.Size(75, 80);
+            this.teacherButton.TabIndex = 0;
+            this.teacherButton.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.teacherButton.UseVisualStyleBackColor = true;
+            // 
+            // studentButton
+            // 
+            this.studentButton.ImageKey = "student.png";
+            this.studentButton.ImageList = this.imageList;
+            this.studentButton.Location = new System.Drawing.Point(8, 154);
+            this.studentButton.Name = "studentButton";
+            this.studentButton.Size = new System.Drawing.Size(75, 80);
+            this.studentButton.TabIndex = 1;
+            this.studentButton.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.studentButton.UseVisualStyleBackColor = true;
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "student.png");
+            this.imageList.Images.SetKeyName(1, "teacher.png");
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.AutoScroll = true;
+            this.flowLayoutPanel.Controls.Add(this.panel1);
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(683, 450);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.emailLabel);
+            this.panel1.Controls.Add(this.photoGroupBox);
+            this.panel1.Controls.Add(this.toolStrip);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 139);
+            this.panel1.TabIndex = 0;
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripLabel});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStrip.Size = new System.Drawing.Size(183, 25);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::DynamicPeople.Properties.Resources.plus;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripLabel
+            // 
+            this.toolStripLabel.AutoSize = false;
+            this.toolStripLabel.IsLink = true;
+            this.toolStripLabel.Name = "toolStripLabel";
+            this.toolStripLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripLabel.Size = new System.Drawing.Size(140, 22);
+            this.toolStripLabel.Text = "toolStripLabel1";
+            this.toolStripLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.Location = new System.Drawing.Point(3, 25);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(177, 23);
+            this.emailLabel.TabIndex = 1;
+            this.emailLabel.Text = "label1";
+            // 
+            // photoGroupBox
+            // 
+            this.photoGroupBox.Controls.Add(this.photoPictureBox);
+            this.photoGroupBox.Location = new System.Drawing.Point(6, 46);
+            this.photoGroupBox.Name = "photoGroupBox";
+            this.photoGroupBox.Size = new System.Drawing.Size(92, 87);
+            this.photoGroupBox.TabIndex = 52;
+            this.photoGroupBox.TabStop = false;
+            this.photoGroupBox.Text = "Photo";
+            // 
+            // photoPictureBox
+            // 
+            this.photoPictureBox.BackColor = System.Drawing.Color.LightGray;
+            this.photoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.photoPictureBox.Location = new System.Drawing.Point(3, 16);
+            this.photoPictureBox.Name = "photoPictureBox";
+            this.photoPictureBox.Size = new System.Drawing.Size(86, 68);
+            this.photoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.photoPictureBox.TabIndex = 0;
+            this.photoPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Dynamic People";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.flowLayoutPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.photoGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button studentButton;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Button teacherButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.GroupBox photoGroupBox;
+        private System.Windows.Forms.PictureBox photoPictureBox;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel;
     }
 }
 
